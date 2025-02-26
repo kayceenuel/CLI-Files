2e62f2efb3292748ca4cc00891a961fc49f9eec0
Kaycee Nuel <heykayceee@gmail.com>
2025-02-26 10:50:14 +0100

    added the Readfile arugments

--- go-cat/cmd/root.go
+++ go-cat/cmd/root.go
@@ -14,4 +14,10 @@ func ReadFiles() {
 		fmt.Println("Missing parameter, provide file name!")
 		return
 	}
+	data, err := os.ReadFile(os.Args[1])
+	if err != nil {
+		fmt.Println("Can't read files:", os.Args[1])
+		return
+	}
+	os.Stderr.Write(data)
 }
