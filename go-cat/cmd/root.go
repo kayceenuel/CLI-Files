d37a60b5854a34345e5426a2688bda0f55039fce
Kaycee Nuel <heykayceee@gmail.com>
2025-02-26 10:39:08 +0100

    fixing func err

--- go-cat/cmd/root.go
+++ go-cat/cmd/root.go
@@ -2,24 +2,16 @@ package cmd
 
 import (
 	"fmt"
-	"io/ioutil"
 	"os"
 )
 
 func Execute() {
-	Readfiles()
+	ReadFiles()
 }
 
 func ReadFiles() {
-	if len(os.Args) > 1 {
+	if len(os.Args) <= 1 {
 		fmt.Println("Missing parameter, provide file name!")
 		return
 	}
-	data, err := ioutil.ReadFile(os.Args[1])
-	if err != nil {
-		fmt.Println("Cant't read file:", os.Args[1])
-		panic(err)
-	}
-	fmt.Println("File content is:")
-	os.Stdout.Write(strings(data))
 }
