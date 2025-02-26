af10b40ca6d5806733dfc43e8b30b885ea517be2
Kaycee Nuel <heykayceee@gmail.com>
2025-02-26 10:13:16 +0100

    added readfile logic

--- go-cat/cmd/root.go
+++ go-cat/cmd/root.go
@@ -1,3 +1,25 @@
 package cmd
 
-func Execute() {}
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+func Execute() {
+	Readfiles()
+}
+
+func ReadFiles() {
+	if len(os.Args) > 1 {
+		fmt.Println("Missing parameter, provide file name!")
+		return
+	}
+	data, err := ioutil.ReadFile(os.Args[1])
+	if err != nil {
+		fmt.Println("Cant't read file:", os.Args[1])
+		panic(err)
+	}
+	fmt.Println("File content is:")
+	os.Stdout.Write(strings(data))
+}
