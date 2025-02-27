8537d9d4177e0eb2130f78e7b7f9af46ec5fb308
Kaycee Nuel <heykayceee@gmail.com>
2025-02-27 16:38:29 +0100

    I think i fixed it this time lol

--- go-cat/cmd/root.go
+++ go-cat/cmd/root.go
@@ -14,22 +14,21 @@ func ReadFiles() {
 		fmt.Println("Missing parameter, provide file name!")
 		return
 	}
-	// loop over arguments
 	for _, file := range os.Args[1:] {
 		info, err := os.Stat(file)
 		if err != nil {
 			fmt.Println("go-cat: ", file, ": ", err)
-			return
+			continue
 		}
-		if !info.IsDir() {
+		if info.IsDir() {
 			fmt.Println("go-cat: ", file, ": is a directory")
+			continue
+		}
+		data, err := os.ReadFile(os.Args[1])
+		if err != nil {
+			fmt.Println("Can't read files:", os.Args[1])
 			return
 		}
+		os.Stdout.Write(data)
 	}
-	data, err := os.ReadFile(os.Args[1])
-	if err != nil {
-		fmt.Println("Can't read files:", os.Args[1])
-		return
-	}
-	os.Stderr.Write(data)
 }
