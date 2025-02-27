dfdbf1dbc9dfc89cb39d3c995f6fd53ef16b8dc5
Kaycee Nuel <heykayceee@gmail.com>
2025-02-27 15:55:46 +0100

    added loop to the args and fixing buggys

--- go-cat/cmd/root.go
+++ go-cat/cmd/root.go
@@ -14,6 +14,18 @@ func ReadFiles() {
 		fmt.Println("Missing parameter, provide file name!")
 		return
 	}
+	// loop over arguments
+	for _, file := range os.Args[1:] {
+		info, err := os.Args(file)
+		if err != nil {
+			fmt.Println(file.Name())
+			return
+		}
+		if !info.IsDir() {
+			fmt.Println("go-cat: <filePath>: is a directory", os.Args[1])
+			return
+		}
+	}
 	data, err := os.ReadFile(os.Args[1])
 	if err != nil {
 		fmt.Println("Can't read files:", os.Args[1])
